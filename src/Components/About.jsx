7c2a3c77bf16c08228b57972e35e88caf81ee7ee
Tanvi Jain <tanvijain1511@gmail.com>
2024-03-30 16:42:07 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/smoke.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Smokey background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  " I'm Tanvi Jain, a pre-final year CSE undergraduate, with a fervent interest in both technology and the creative realm. Beyond my love for coding, I find joy in dance, speaking, and expressing myself creatively. I thrive on managing events and tasks efficiently, bringing my organizational skills to the forefront. Let's explore the intersection of technology and the arts together, and turn ideas into reality!";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Development",
+  "JAVA",
+  "DSA",
+  "Leadership",
+  "Communication",
+  "Designing",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Welcome to my portfolio!. I'm thrilled to have you here. Let's embark on a journey together, explore possibilities, and craft something extraordinary. ";
 
 const About = () => {
   return (
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/moon.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "In the dance of light and shadow, we mirror the moon, revealing our brightest radiance alongside our hidden depths";
 
 const Home = ({ name, title }) => {
   return (
